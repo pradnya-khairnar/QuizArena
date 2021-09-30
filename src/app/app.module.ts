2632669cc7945f89b49bc5b6726a57fb62ea762e
Pradnya Khairnar <khairnarpradnya5@gmail.com>
2021-09-30 18:47:17 +0530

    code clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HostComponent } from './host/host.component';
-import { UsersComponent } from './users/users.component';
 import { QueAnsComponent } from './que-ans/que-ans.component';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
 
 const config: SocketIoConfig = { url: 'http://localhost:4201', options: {} };
 @NgModule({
-  declarations: [AppComponent, HostComponent, UsersComponent, QueAnsComponent],
+  declarations: [AppComponent, QueAnsComponent],
   imports: [BrowserModule, AppRoutingModule, SocketIoModule.forRoot(config)],
   providers: [],
   bootstrap: [AppComponent],
